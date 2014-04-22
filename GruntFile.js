aa25c6a1e9b7145405ba431f91e99ebd9fc4150c
odoe <odoe@odoe.net>
2014-04-22 13:33:50 -0700

    added grunt-contrib-watch with a testunner.html to use livereload

--- GruntFile.js
+++ GruntFile.js
@@ -7,11 +7,18 @@ module.exports = function(grunt) {
                     config: 'tests/intern'
                 }
             }
+        },
+        watch: {
+            all: {
+                options: { livereload: true },
+                files: ['app/*.js', 'gis/**/*.js', 'gis/**/**/*.js']
+            }
         }
     });
 
     // Loading using a local copy
     grunt.loadNpmTasks('intern');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Register a test task
     grunt.registerTask('test', ['intern']);
